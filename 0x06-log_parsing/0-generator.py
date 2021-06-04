2ac21e3cdb30a28c184ac61c51702e640d30d46c
Fernando <sandovbarr@gmail.com>
2021-06-03 20:08:44 -0500

    [ADD] project files

---
+++ 0x06-log_parsing/0-generator.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import random
+import sys
+from time import sleep
+import datetime
+
+for i in range(3):
+    sys.stdout.write('78.99.227.220 - [2017-02-05 23:25:51.534767] "GET /projects/260 HTTP/1.1" 401 724\n')
+    sys.stdout.flush()
